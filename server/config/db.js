503d8d109b7d1a7d05251f736f6a5e176cba4be2
vishalreddyk <2400080133@kluniversity.in>
2026-05-20 15:53:23 +0530

    Support SSL MySQL connections

--- server/config/db.js
+++ server/config/db.js
@@ -5,11 +5,13 @@ const DB_USER = process.env.DB_USER || 'root';
 const DB_PASS = process.env.DB_PASS || '';
 const DB_HOST = process.env.DB_HOST || '127.0.0.1';
 const DB_PORT = process.env.DB_PORT || 3306;
+const DB_SSL = process.env.DB_SSL === 'true';
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
   host: DB_HOST,
   port: DB_PORT,
   dialect: 'mysql',
+  dialectOptions: DB_SSL ? { ssl: { require: true, rejectUnauthorized: false } } : {},
   logging: false
 });
 
